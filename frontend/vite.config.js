3f31a7a30a2fbaa0234ca601faa4610e1b978c3e
phessophissy <adefesobi@yahoo.com>
2025-12-23 14:18:42 +0100

    feat: Add frontend project structure and HTML

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  define: {
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer'
+    }
+  }
+});
